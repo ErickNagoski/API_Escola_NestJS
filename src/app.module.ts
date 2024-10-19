f1dbb863aaa76058d656284260bf43e2d923086b
Erick Nagoski <ericknagoski@gmail.com>
2024-10-18 21:49:46 -0300

    Feat: Entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlunosModule } from './alunos/alunos.module';
+import { DisciplinasModule } from './disciplinas/disciplinas.module';
 
 @Module({
-  imports: [],
+  imports: [AlunosModule, DisciplinasModule],
   controllers: [AppController],
   providers: [AppService],
 })
